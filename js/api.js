292e6b8fb0aa85e896a30638f99020ecd980f86e
dreamsaboutlana <Sveta@worker.local>
2017-07-29 16:20:41 +0300

    Add api.js (not ready yet)

---
+++ js/api.js
@@ -0,0 +1,23 @@
+class Api {
+  constructor(url) {
+    this.url = url;
+  }
+  requestUser() {
+    return fetch(this.url)
+      .then(data => data.json())
+      .then(data => console.log(data));
+  }
+  postUser() {
+    return fetch(url, {
+        method: "POST",
+        body: JSON.stringify(this.newUser),
+        headers: { "Content-Type": "application/json" },
+      })
+      .then(data => {
+        myPhoneBook.request();
+      })
+  }
+}
+const api = new Api('http://easycode-js.herokuapp.com/dreamsaboutlana/users');
+// const api = new Api(url + 'user');
+console.log(api.requestUser());
